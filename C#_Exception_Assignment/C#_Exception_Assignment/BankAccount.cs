88cea71c3ff6ecc94a25b4d6bd1a6daeb2e34bf4
Garimab-12 <128513628+Garimab-12@users.noreply.github.com>
2026-03-18 12:50:42 +0530

    Add files via upload

---
+++ C#_Exception_Assignment/C#_Exception_Assignment/BankAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__Exception_Assignment
+{
+    internal class BankAccount
+    {
+    }
+}
---
+++ C#_Exception_Assignment/C#_Exception_Assignment/MovieTicket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MovieTicketApp
+{
+    class TicketException : Exception
+    {
+        public TicketException(string message) : base(message)
+        {
+        }
+    }
+
+    class MovieTicketProgram
+    {
+        static void Main()
+        {
+            int availableTickets = 15;
+
+            try
+            {
+                Console.WriteLine("Do you want to book tickets? (yes/no)");
+                string choice = Console.ReadLine();
+
+                if (choice.ToLower() == "yes")
+                {
+                    Console.WriteLine("Enter number of tickets:");
+                    int tickets = Convert.ToInt32(Console.ReadLine());
+
+                    if (tickets > availableTickets)
+                    {
+                        throw new TicketException("Not enough tickets available!");
+                    }
+
+                    availableTickets -= tickets;
+
+                    Console.WriteLine("Booking successful!");
+                    Console.WriteLine("Remaining tickets: " + availableTickets);
+                }
+                else
+                {
+                    Console.WriteLine("Thank you!");
+                }
+            }
+            catch (TicketException ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
